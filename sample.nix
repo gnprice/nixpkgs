818e07f433c4c325b85c12edee1400acb0eef7f8
Greg Price <gnprice@gmail.com>
2020-04-09 21:43:31 -0700

    scaffold: handle packages w/ custom stdenv
    E.g. `palemoon`, which came up in a random sample of 10 callers
    of stdenv.mkDerivation.

--- sample.nix
+++ sample.nix
@@ -14,13 +14,16 @@ let
   graftStdenv = base: scripts: scripts.override {
     inherit (base) initialPath cc shell extraNativeBuildInputs;
     allowedRequisites =
-      builtins.filter (p: !(lib.hasSuffix ".sh" p))
-        base.allowedRequisites;
+      if base ? allowedRequisites then
+        builtins.filter (p: !(lib.hasSuffix ".sh" p))
+          base.allowedRequisites
+      else null;
   };
 
   devStdenv = graftStdenv nixpkgs.stdenv devpkgs.stdenv;
 in
 
-(lib.getAttrFromPath (lib.splitString "." attr) nixpkgs).override {
-  stdenv = devStdenv;
-}
+(lib.getAttrFromPath (lib.splitString "." attr) nixpkgs).override (
+  old: {
+    stdenv = graftStdenv old.stdenv devpkgs.stdenv;
+  })
