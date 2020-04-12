120dec2f3155c9a09e5d8d94d906652f0d5f445d
Greg Price <gnprice@gmail.com>
2020-04-11 19:53:13 -0700

    scaffold: add comments

--- sample.nix
+++ sample.nix
@@ -1,7 +1,15 @@
 { attr
 }:
 
-# usage:
+# Scaffold to build just one package with modified stdenv scripts.
+#
+# Dependencies come unmodified from <nixpkgs>, including the stdenv's
+# toolchain.  This is useful for (a) iterating quickly on changes to
+# stdenv scripts (like setup.sh), and (b) more easily comparing the
+# build log against a baseline, by keeping inputs' paths the same so
+# that any changes caused by changes in the scripts' behavior stand out.
+#
+# Usage:
 # $ nix-shell -p $(nix-build $(nix-instantiate sample.nix \
 #     --argstr attr netmask))
 
@@ -11,6 +19,7 @@ let
 
   inherit (devpkgs) lib;
 
+  # Graft the scripts from one stdenv onto the toolchain from another.
   graftStdenv = base: scripts: scripts.override {
     inherit (base) initialPath cc shell extraNativeBuildInputs;
     allowedRequisites =
